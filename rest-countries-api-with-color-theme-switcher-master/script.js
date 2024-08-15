6ce6248c08c9e8b379ab9ae1b5f3b72f0d4d269d
Trishachi <trisha.omabu@gmail.com>
2024-08-15 00:00:05 -0600

    Fetch Payload

---
+++ rest-countries-api-with-color-theme-switcher-master/script.js
@@ -0,0 +1,19 @@
+// Fetch Data from endpoint
+
+function fetchJSONData() {
+    fetch("data.json")
+        .then((res) => {
+            if (!res.ok) {
+                throw new Error
+                    (`HTTP error! Status: ${res.status}`);
+            }
+            return res.json();
+        })
+        // Fetch request successful
+        .then((data) => 
+              console.log(data[0].borders))
+        // Fetch Request failed
+        .catch((error) => 
+               console.error("Unable to fetch data:", error));
+}
+fetchJSONData();
