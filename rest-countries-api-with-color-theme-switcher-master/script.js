e78e776e96040b31cce389ba67e2e1549da313c6
Trishachi <trisha.omabu@gmail.com>
2024-09-10 22:53:02 -0600

    Card display WIP

--- rest-countries-api-with-color-theme-switcher-master/script.js
+++ rest-countries-api-with-color-theme-switcher-master/script.js
@@ -12,25 +12,39 @@ function fetchJSONData() {
         // Fetch request successful
         .then(data => {
             console.log(data);
+            let cardsHTML = '';
             data.forEach((country) => {
                 //console.log(country.flags.svg)
 
                 //Create HTML Elements 
-                let image = document.createElement("img");
-                image.src = country.flags.svg;
-                let title = document.createElement("h2");
-                title.innerText = country.name.common;
-                let population = document.createElement("p");
-                population.innerText = country.population;
-                let region = document.createElement("p");
-                region.innerText = country.region;
-                let capital =  document.createElement("p");
-                capital.innerText = country.capital;
+                // let image = document.createElement("img");
+                // image.src = country.flags.svg;
+                // let title = document.createElement("h2");
+                // title.innerText = country.name.common;
+                // let population = document.createElement("p");
+                // population.innerText = country.population;
+                // let region = document.createElement("p");
+                // region.innerText = country.region;
+                // let capital =  document.createElement("p");
+                // capital.innerText = country.capital;
 
                 //Create card and add data to card
-                
+                const {flags, name, population} = country;
+                cardsHTML += `
+                <div class="container">
+                    <div class="card" style="width: 18rem;">
+                        <img class="card-img-top" src="${flags.svg}" alt="Flag of ${name.common}">
+                        <div class="card-body">
+                            <h2 class="card-title">${name.common}</h2>
+                            <p class="card-text">Population: ${population.toLocaleString()}</p>
+                        </div>
+                        
+                    </div>
+                </div>
+                `
         
-                document.getElementById('results').appendChild(capital);
+                // document.getElementById('results').appendChild(capital);
+                document.getElementById('results').innerHTML = cardsHTML;
             }
         );
 
